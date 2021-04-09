f4776d26796e0811b70bbe9eddeb34b0bf8d7898
a-phung <andyphunghb@gmail.com>
2021-04-08 20:47:14 -0700

    Wrote code to take user input of a ticker symbol and future date. Wrote logic to determine if user inputted an appropriate date. To write logic for ticker symbol input.

---
+++ stock_prediction.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import yfinance as yf
+from datetime import date, datetime, timedelta
+
+# Get user input: one stock ticker and a time horizon of a min of one day and max of six months.
+ticker = input("Please enter a stock symbol or ticker: ").upper()
+
+future_date = input("Please enter a future time up to six months from the present in the format "
+                    "'YYYY-MM-DD' to predict the price on that date: ")
+
+while True:
+    def date_check(user_date):
+        """Verifies that the date inputted by user is less than six months."""
+        today = date.today()
+        six_months = today + timedelta(days=180)
+        if len(str(today)) != len(user_date):
+            return False
+        if user_date[4] != "-" or user_date[7] != "-":
+            return False
+
+        future_year = int(user_date[:4])
+        future_month = int(user_date[5:7])
+        future_day = int(user_date[8:])
+
+        past_date = today > datetime(future_year, future_month, future_day).date()
+        past_max = six_months < datetime(future_year, future_month, future_day).date()
+
+        if past_date or past_max:
+            return False
+        else:
+            return True
+
+    result = date_check(future_date)
+    if result:
+        break
+    elif not result:
+        future_date = input("Please reenter a future time up to six months from the present "
+                            "in the format 'YYYY-MM-DD' to predict the price on that date: ")
+
+
