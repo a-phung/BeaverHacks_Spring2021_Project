d82297fb37c86bb6dbf8aa96b815098e7e02c3d1
Noah Bean <32600254+ThunderChief33@users.noreply.github.com>
2021-04-11 13:57:46 -0700

    fixed terminal bug

--- mla.py
+++ mla.py
@@ -9,7 +9,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
 #load data
-company = 'TSLA'
+company = 'GOOGL'
 start = dt.datetime(2012,1,1)
 end = dt.datetime(2020,1,1)
 
@@ -20,7 +20,7 @@ scaler = MinMaxScaler(feature_range = (0,1))
 scaled_data = scaler.fit_transform(data['Adj Close'].values.reshape(-1,1))
 #uses the adjusted close price
 
-prediction_days = 60
+prediction_days = 100
 
 x_train = []
 y_train = []
@@ -33,8 +33,10 @@ x_train = np.array(x_train)
 y_train = np.array(y_train)
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
-
+#print(len(x_train))
 #build model (simple tree)
+
+
 model = Sequential()
 #https://www.tensorflow.org/api_docs/python/tf/keras/Sequential
 model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1],1)))
@@ -45,10 +47,11 @@ model.add(LSTM(units = 50))
 model.add(Dropout(0.2))
 model.add(Dense(units = 1)) #prediction of next closing day
 
-model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+model.compile(optimizer = 'rmsprop', loss = 'mean_squared_error')
 
 #fitting
-model.fit(x_train, y_train, epochs = 25, batch_size = 32)
+model.fit(x_train, y_train, epochs = 50, batch_size = None)
+print(model.summary())
 
 #model accuracy
 
@@ -62,9 +65,9 @@ total_dataset = pd.concat((data['Adj Close'], test_data['Adj Close']), axis = 0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
 model_inputs = model_inputs.reshape(-1,1)
-model.inputs = scaler.transform(model_inputs)
+model_inputs = scaler.transform(model_inputs)
 
-print(model.summary())
+
 #print(model.evaluate())
 
 # make predictions on test data
@@ -74,28 +77,30 @@ for x in range(prediction_days, len(model_inputs)):
     x_test.append(model_inputs[x-prediction_days:x,0])
 
 x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-#x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
+#print(len(x_test))
 
-predicted_prices = model.predict(x_test)
-#predicted_prices = scaler.inverse_transform(predicted_prices)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+predicted_prices = model.predict(x_test, batch_size=None)
+predicted_prices = scaler.inverse_transform(predicted_prices)
 
 #plotting
-plt.plot(actual_prices, color = 'black', label= 'Actual Price')
-#plt.plot(predicted_prices, color = 'red', label= 'Predicted Price')
+plt.plot(actual_prices, color = 'black', label = 'Actual Price')
+plt.plot(predicted_prices, color = 'red', label = 'Predicted Price')
 plt.title('Share Price')
-plt.xlabel('Time')
-plt.ylabel('Share Price')
+plt.xlabel('Time (days)')
+plt.ylabel('Share Price (USD)')
 plt.legend()
-plt.show()
+
 
 
 real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs + 1), 0]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
 
-#prediction = model.predict(real_data)
-#prediction = scaler.inverse_transform(prediction)
+prediction = model.predict(real_data, batch_size=None)
+prediction = scaler.inverse_transform(prediction)
 print('prediction for next day is')
-#print(prediction)
+print(prediction)
 
+plt.show()
